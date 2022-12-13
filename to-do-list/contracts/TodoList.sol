df238322368bafee5bb2f0e93e62603983e11cca
Nikunj Garg <nikunjgarg2000@gmail.com>
2022-12-13 13:15:20 +0530

    Lists Tasks

--- to-do-list/contracts/TodoList.sol
+++ to-do-list/contracts/TodoList.sol
@@ -1,6 +1,26 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.5.0;
 
 contract TodoList {
     // state variable
     uint public taskCount = 0;
+
+    struct Task {
+        uint id;
+        string content;
+        bool completed;
+    }
+
+    mapping(uint => Task) public tasks;
+
+    constructor() public {
+        createTask("Check you out!");
+    }
+
+    function createTask(string memory _content) public {
+        taskCount++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+    }
+
+
 }
